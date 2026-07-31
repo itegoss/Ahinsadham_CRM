98b264ad81963e39c556c04d1f677e4f0fd6ab5d
varsha <varsha@itegoss.in>
2026-07-31 12:46:07 +0530

    helppp1

--- ngo/settings.py
+++ ngo/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = "ngo.wsgi.application"
 import os
 ENV = os.getenv("ENV", "DEV")
 DATABASES = {
-             'default': {
-                 'ENGINE': 'django.db.backends.postgresql',
-                 'NAME': os.getenv('DB_NAME'),
-                 'USER': os.getenv('DB_USER'),
-                 'PASSWORD': os.getenv('DB_PASSWORD'),
-                 'HOST': os.getenv('DB_HOST'),
-                 'PORT': os.getenv('DB_PORT'),
-     }
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
 
 
 
